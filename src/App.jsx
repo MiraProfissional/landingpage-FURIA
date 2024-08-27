82991e3d2f906343ca8e9a7368a40c7a1550bc3f
MiraProfissional <jvitor.gmira@gmail.com>
2024-08-27 20:00:55 -0300

    Preparação do ambiente

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>Projeto Root</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<p>Projeto Root</p>
+		</div>
+	);
 }
 
 export default App;
