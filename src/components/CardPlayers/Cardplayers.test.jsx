d46a6fbd713b7f8b64ef266ef02b95dcf928174a
MiraProfissional <jvitor.gmira@gmail.com>
2024-08-28 17:09:37 -0300

    Ajuste no teste do componente 'CardPlayer'

--- src/components/CardPlayers/Cardplayers.test.jsx
+++ src/components/CardPlayers/Cardplayers.test.jsx
@@ -1,8 +1,18 @@
-import { render } from "@testing-library/react";
+import { render,screen } from "@testing-library/react";
 import CardPlayer from ".";
 
 describe("</ CardPlayer>", () => {
 	it("should render </CardPlayer>", () => {
-		render(<CardPlayer />);
+		const mockData = {
+			imagem: 'caminho/para/imagem.png',
+			nome: 'João Vitor',
+			posicao: 'Atacante',
+			capitao: 'true',
+			nacionalidade: 'Brasileira',
+			nascimento: '01/01/2000'
+		};
+
+		render(<CardPlayer data={mockData} />);
+    
 	});
 });
